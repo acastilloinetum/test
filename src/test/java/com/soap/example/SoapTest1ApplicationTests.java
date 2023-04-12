403ecf6c62ba4d02f9eb7fef7ac76fb29b2c56ab
Antonio <antonio-jose.castillo@inetum.com>
2023-04-12 15:25:42 +0200

    aa

---
+++ src/test/java/com/soap/example/SoapTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
